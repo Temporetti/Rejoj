4d711dc121371ac6cc5217ffac11b9d9a9712980
Temporetti <valentintemporetti@hotmail.com>
2021-08-11 00:52:19 -0300

    Avance en el proyecto del reloj. Imprime el circulo con el patron dentro

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,57 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    dibujoReloj = new QPaintBox(0,0,ui->widget); //!< Creo el objeto dibujoReloj que es el widget donde se dibuja (instanciar)
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+
+    delete dibujoReloj;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    int h;
+    int w;
+    int radio;
+
+    QPen pen;
+    QBrush brush;
+    QPainter paint(dibujoReloj->getCanvas());
+
+    w = dibujoReloj->width();
+    h = dibujoReloj->height();
+
+    radio = w/2;
+
+    if(h < w){
+        radio = h/2;
+    }
+
+    //seteo el color y la herramienta
+    pen.setColor(Qt::white);
+    paint.setPen(pen);  //aplica como los valores que puse en la variable pen del tipo Qpen a la herramienta para
+                           //dibujar con pen?
+
+    brush.setColor(Qt::gray);
+    brush.setStyle(Qt::Dense4Pattern);
+    paint.setBrush(brush);  //aplica como los valores que puse en la variable brush del tipo Qbrush a la herramienta para
+                            //dibujar con brush?
+
+    //dibujo
+    paint.drawEllipse(w/2-radio, h/2-radio, 2*radio, 2*radio);
+
+
+
+    //actualizo para que se vean los cambios
+    dibujoReloj->update();
+
+
 }
 
