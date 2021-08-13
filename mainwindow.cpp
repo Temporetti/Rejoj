b40e9424b1153b06a11c384cdeca323ffc308093
Temporetti <valentintemporetti@hotmail.com>
2021-08-13 19:14:10 -0300

    13/8/2021 mejora en el reloj

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,55 +7,82 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    dibujoReloj = new QPaintBox(0,0,ui->widget); //!< Creo el objeto dibujoReloj que es el widget donde se dibuja (instanciar)
-
+    dibujar = new QPaintBox(0,0,ui->widget); //!< Creo el objeto dibujoReloj que es el widget donde se dibuja (instanciar)
+    QTimer1 = new QTimer(this);
+    connect(QTimer1, &QTimer::timeout, this, &MainWindow::onQTimer1);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 
-    delete dibujoReloj;
+    delete dibujar;
+    delete QTimer1;
 }
 
-
-void MainWindow::on_pushButton_clicked()
-{
-
+//aca desarrollar la funcion
+void MainWindow::dibujarReloj(){
     int h;
     int w;
     int radio;
 
     QPen pen;
     QBrush brush;
-    QPainter paint(dibujoReloj->getCanvas());
+    QPainter paint(dibujar->getCanvas());
 
-    w = dibujoReloj->width();
-    h = dibujoReloj->height();
+    w = dibujar->width();
+    h = dibujar->height();
 
-    radio = w/2;
+    radio = (w/2)-10;
 
     if(h < w){
-        radio = h/2;
+        radio = (h/2)-10;
     }
 
     //seteo el color y la herramienta
+    pen.setWidth(3);
     pen.setColor(Qt::white);
     paint.setPen(pen);  //aplica como los valores que puse en la variable pen del tipo Qpen a la herramienta para
-                           //dibujar con pen?
+                        //dibujar con pen?
 
     brush.setColor(Qt::gray);
     brush.setStyle(Qt::Dense4Pattern);
     paint.setBrush(brush);  //aplica como los valores que puse en la variable brush del tipo Qbrush a la herramienta para
                             //dibujar con brush?
 
-    //dibujo
+    //dibujo circulo
     paint.drawEllipse(w/2-radio, h/2-radio, 2*radio, 2*radio);
 
+}
+
+void MainWindow::onQTimer1(){
+
+}
 
 
+void MainWindow::on_pushButton_clicked()
+{
+    QDateTime dt;
+    int horas;
+    int minutos;
+    int segundos;
+
+    if(QTimer1->isActive()){
+        QTimer1->stop();
+        ui->pushButton->setText("START");
+    }else{
+        QTimer1->start(100);    //!< 100 por el tiempo de actualizacin de agujas. Donde hago define?
+        ui->pushButton->setText("STOP");
+    }
+
+    dibujarReloj();
     //actualizo para que se vean los cambios
-    dibujoReloj->update();
+    dibujar->update();
+
+    horas = dt.currentDateTime().time().hour();
+    minutos = dt.currentDateTime().time().minute();
+    segundos = dt.currentDateTime().time().second();
+
 
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QTimer>
 #include <qpaintbox.h>
 #include <QDateTime>
+//define aca
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,10 +21,20 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+    void onQTimer1();
 
 private:
     Ui::MainWindow *ui;
 
-    QPaintBox *dibujoReloj;  //!< Declaro la variable de tipo QPaintBox para dibujar el reloj?
+    QPaintBox *dibujar;  //!< Declaro la variable de tipo QPaintBox para dibujar en el widget?
+    QTimer *QTimer1;
+
+    /*!
+     * \brief dibujarReloj
+     * Dibuja el circulo del reloj con su patron.
+     */
+    void dibujarReloj();
+
+
 };
 #endif // MAINWINDOW_H
